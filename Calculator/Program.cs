574cf11f2027ebc81ece057c170834f03ae8025e
Olavi Raigla <oraigla@tktk.ee>
2024-04-22 10:54:08 +0300

    add week 7 code

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Enter the operation (+/-):");
+char userOperator = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a:");
+int firstNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter b:");
+int secondNum = int.Parse(Console.ReadLine());
+
+switch (userOperator)
+{
+    case '+':
+        Addition(firstNum, secondNum);
+        break;
+    case '-':
+        Subtraction(firstNum, secondNum);
+        break;
+    default:
+        Console.WriteLine("invalid operator");
+        break;
+}
+
+static void Addition(int a, int b) //võtab argumentidena sisse a ja b
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void Subtraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
+
+//void = vahemälusse ei jää peale arvutust mitte midagi
---
+++ DiscountCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿//arvutab allahindlust
+//total < kui 10, siis allahindlus on - 1%
+//total >= 10 && 20 < 20 - 5%
+//total >= 20 - 10%
+
+Console.WriteLine("Provide total: ");
+int userInput = Int32.Parse(Console.ReadLine());
+
+double discount = CalculateDiscount(userInput);
+
+Console.WriteLine($"Your discount is: {discount} %");
+
+double newTotal = CalculateNewTotal(userInput, discount);
+
+Console.WriteLine($"Your updated total with {discount}%: {newTotal}");
+
+static double CalculateDiscount(int total)
+{
+    if(total < 10)
+    {
+        return 1;
+    }
+    else if(total >= 10 && total < 20)
+    {
+        return 5;
+    }
+    else
+    {
+        return 10;
+    }
+}
+//double on siis andmed ühe komakohaga ehk võib kirjutada return 1,0 jne
+
+static double CalculateNewTotal(double total, double discount)
+{
+    double result = total - (total * discount) / 100;
+    return result;
+} 
+
+//static tähendab iseseisvat funktsiooni
